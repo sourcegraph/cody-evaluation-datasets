8fd7fa3bfe6fc30640af69b3e4b8b32753c86fef
Tom Ross <tom@umpox.com>
2023-10-03 09:33:19 +0100

    Add single line dataset

--- HumanEval/Infilling/24/generated.py
+++
@@ -1,9 +0,0 @@
-
-
-
-def largest_divisor(n: int) -> int:
-    """ For a given number n, find the largest number that divides n evenly, smaller than n
-    >>> largest_divisor(15)
-    5
-    """
-    for i in r🔥
